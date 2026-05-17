e5b1e9f89dcdec4e3110e919b4e0391f09a58c5f
lujji <lujji@protonmail.com>
2026-05-17 23:38:49 +0300

    yolo

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,12 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+Reads 8 inputs and spits them out to UART.
 
 ## How to test
 
-Explain how to use your project
+Connect via UART@1000000, observe pin states and enjoy.
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+UART receiver connected to uo[0].
--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_lujji_ulogic_analyzer (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,31 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+// All output pins must be assigned. If not used, assign to 0.
+assign uo_out[7:2] = 0;
+assign uio_out = 0;
+assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+assign uo_out[1] = clk; // clk out
+
+// List all unused inputs to prevent warnings
+wire _unused = &{ena, uio_in, 1'b0};
+wire reset = ~rst_n;
+
+// UART TX module
+wire tx_done;
+reg [7:0] txd;
+wire tx_trigger = 1'b1; // yolo..
+
+always @(posedge clk)
+if (reset) begin
+  txd <= 8'b0;
+end else begin
+  if (tx_done == 1'b1) begin
+     txd <= ui_in;
+  end
+end
+
+uart_tx #(.clk_freq (50000000)) UART_TX_inst(clk, tx_trigger, txd, uo_out[0], tx_done);
 
 endmodule
