865f4aa2d859d595911bddf00930e892e32dfb35
lujji <lujji@protonmail.com>
2026-05-17 23:41:13 +0300

    fix top module name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_lujji_ulogic_analyzer (
+module tt_um_lujji_ulogic_analyzer (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
