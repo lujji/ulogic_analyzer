044caa80399eded2788a2fae6891c4354a92e77f
lujji <lujji@protonmail.com>
2026-05-17 23:46:37 +0300

    "fix" tests..

--- test/tb.v
+++ test/tb.v
@@ -6,11 +6,9 @@
 */
 module tb ();
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.fst");
     $dumpvars(0, tb);
-    #1;
   end
 
   // Wire up the inputs and outputs:
@@ -22,28 +20,50 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
+
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
-      // Include power ports for the Gate Level test:
+  tt_lujji_ulogic_analyzer dut (
 `ifdef GL_TEST
       .VPWR(VPWR),
       .VGND(VGND),
 `endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+      .ui_in(ui_in),
+      .uo_out(uo_out),
+      .uio_in(uio_in),
+      .uio_out(uio_out),
+      .uio_oe(uio_oe),
+      .ena(ena),
+      .clk(clk),
+      .rst_n(rst_n)
   );
+
+  initial clk = 0;
+  always #10 clk = ~clk;
+
+  initial begin
+    ena = 1'b1;
+    rst_n = 1'b0;
+    ui_in = 8'h55;
+    uio_in = 8'h00;
+
+    #100;
+    rst_n = 1'b1;
+
+    #500000;
+
+    ui_in = 8'hA3;
+
+    #500000;
+
+    ui_in = 8'hF0;
+
+    #500000;
+
+    $finish;
+  end
 
 endmodule
